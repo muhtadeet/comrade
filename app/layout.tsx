3cf6c0db3861023b3c8b614a5abec5b7bba204d5
Muhtadee Taron <sheikh.taron@northsouth.edu>
2024-02-18 03:36:50 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { Header } from '@/components/header'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Comrade',
+    template: `%s - Gemini AI chatbot`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: 'A Gemini-AI powered chatbot.',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
